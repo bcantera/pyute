934d0af9015b4183792af3db82390384f32cae1e
Braihan Cantera <bcanterac@gmail.com>
2022-02-22 23:23:59 -0300

    Uploaded to pypi

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     ),
-    packages=find_packages(),
     include_package_data=True,
     install_requires=["aiohttp"]
 )
