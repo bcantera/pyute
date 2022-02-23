b3f2e0aad6c14b855c87ab88d3a235b2113d54c1
bcantera <45189283+bcantera@users.noreply.github.com>
2022-02-22 22:37:54 -0300

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,22 @@
+import aiohttp
+import asyncio
+from .const import *
+
+class GetToken():
+    async def login(mail, phone):
+        async with aiohttp.ClientSession() as session:
+            LOGIN_HEADERS.update({
+                    "Email": mail,
+                    "PhoneNumber": phone,
+                })
+
+            # Retrieve login token
+            async with session.post(UTE_API_URL + "/v1/token", headers=APP_HEADERS, json=LOGIN_HEADERS) as response:
+                authToken = "Bearer " + await response.text()
+                APP_HEADERS.update({"Authorization": authToken})
+
+                # Check authentication
+                async with session.get(UTE_API_URL + "/v1/accounts", headers=APP_HEADERS) as account_info:
+                    if (account_info.status == 401):
+                        response.status = 401
+                return response
